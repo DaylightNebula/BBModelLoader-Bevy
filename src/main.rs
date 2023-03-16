82a85e01aa751f1a9b1de6f95ca3944417ad8775
DaylightNebula <35437205+DaylightNebula@users.noreply.github.com>
2023-03-16 09:15:06 -0600

    Yayy we have an exit system

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
 use bevy::app::AppExit;
 use bevy::prelude::*;
 use bevy::utils::tracing::Instrument;
+use bevy::winit::WinitSettings;
 
 fn main() {
     App::new()
         .add_state::<AppState>()
         .add_plugins(DefaultPlugins)
-        // .add_plugin(Initializer)
         .add_event::<AppExit>()
-        .add_system(global_update)
-        .add_startup_system(global_init)
-        .add_system(global_exit)
+        .add_system(global_init.in_schedule(OnEnter(AppState::Running)))
+        .add_system(global_update.in_set(OnUpdate(AppState::Running)))
+        .insert_resource(WinitSettings { return_from_run: true, ..default() })
         .run();
+
+    global_exit();
 }
 
-fn global_init() { }
+fn global_init() {
+}
 
 fn global_update() {
+}
+
+fn global_exit() {
 }
 
 #[derive(Default, States, Debug, Clone, Eq, PartialEq, Hash)]
 enum AppState {
     #[default]
-    Init,
     Running
 }
